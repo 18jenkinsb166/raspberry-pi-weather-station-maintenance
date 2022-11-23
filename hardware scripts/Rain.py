a7c5a1d763476d22ed1fa169a5cbb3a184364bee
henry-oldroyd <henryoldroyd10@gmail.com>
2022-11-23 17:44:41 +0000

    de merged rain

--- hardware scripts/Rain.py
+++ hardware scripts/Rain.py
@@ -5,34 +5,17 @@ import time
 import csv
 from typing import Iterable
 
-# removed coma
+# removed comma
 DATE_FORMAT = "%d/%m/%Y %H:%M:%S"
 # Volume of water needed to tip rain gague
 BUCKET_SIZE = 0.2794
 
 #Records Bucket Tip
 def bucketTipped():
-<<<<<<< Updated upstream
-	#Formats current date and time as DD/MM/YY, HH:MM:SS
-	dtNow = datetime.datetime.now()
-	appendMe = dtNow.strftime("%d/%m/%Y, %H:%M:%S")
-
-	rainFile = open("/home/pi/Documents/Integrated Sensors/rainTimesAll.txt", "a") #Opens file to record all rain tips in
-	rainFile.write(appendMe + "\n") #Appends curent time to rain tips file
-	rainFile.close()
-
-#Removes bucket tips from previous day
-def cleanRainfall(dateNow):
-	#Opens the file containing all times of tips, and reads each line
-	rainFile = open("/home/pi/Documents/Integrated Sensors/rainTimesAll.txt", "r")
-	rainTimes = rainFile.readlines()
-	rainFile.close()
-=======
 	now_timestamp: str = datetime.now().strftime(DATE_FORMAT)
 	with open("rain_tip_times.csv", "a", newline="") as csv_file:
 		csv_writer = csv.writer(csv_file, delimiter=",")
 		csv_writer.writerow([now_timestamp])
->>>>>>> Stashed changes
 
 
 def time_difference_less_than_day(now: datetime, then: datetime):
@@ -43,13 +26,6 @@ def time_difference_less_than_day(now: datetime, then: datetime):
 def returnTips():  # sourcery skip: inline-immediately-yielded-variable
 	now: datetime = datetime.now()
 
-<<<<<<< Updated upstream
-		#Writes all valid times to text file, overwriting previously stored tip times
-		recentRainFile = open("/home/pi/Documents/Integrated Sensors/rainTimesAll.txt", "w")
-		for times in validTimes:
-			recentRainFile.write(times)
-		recentRainFile.close()
-=======
 	def generate_tip_timestamps():
 		with open("rain_tip_times.csv", "r") as csv_file:
 			# csv_reader is an iterable of rows
@@ -58,7 +34,6 @@ def returnTips():  # sourcery skip: inline-immediately-yielded-variable
 				time_stamp: str = row[0]
 				time: datetime = datetime.strptime(time_stamp, DATE_FORMAT)
 				yield time
->>>>>>> Stashed changes
 
 	tips: Iterable[datetime] = generate_tip_timestamps()
 	# filter where function true
