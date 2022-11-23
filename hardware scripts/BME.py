27805afa5140d8563e3e243ca648205dd7a4d9c9
SAPosner <98704292+SAPosner@users.noreply.github.com>
2022-11-23 16:27:19 +0000

    Calibration of pressure and humidity

--- hardware scripts/BME.py
+++ hardware scripts/BME.py
@@ -1,6 +1,6 @@
 import time
 
-#Checks right module is installed (could be either of these 2 options)
+# Checks right module is installed (could be either of these 2 options)
 try:
 	from smbus2 import SMBus
 except ImportError:
@@ -12,9 +12,10 @@ def main():
 	bus = SMBus(1)
 	bme280 = BME280(i2c_dev=bus)
 	for i in range(3): # Takes a couple of readings to 'warm up' (otherwise invalid readings collected)
+		# Collects readings and applies appropraite callibration
 		temperature = bme280.get_temperature() - 5
-		pressure = bme280.get_pressure()
-		humidity = bme280.get_humidity()
+		pressure = bme280.get_pressure() + 10
+		humidity = bme280.get_humidity() + 10
 		time.sleep(1)
 
 	return temperature, pressure, humidity
