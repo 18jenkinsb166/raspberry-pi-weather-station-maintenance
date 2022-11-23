40a9b3ad02cd229a60d3372a3c745b5c1c3eb0e1
henry-oldroyd <henryoldroyd10@gmail.com>
2022-11-23 17:16:30 +0000

    my changes to rain

--- hardware scripts/Rain.py
+++ hardware scripts/Rain.py
@@ -1,9 +1,18 @@
 from gpiozero import Button
-import datetime
+from datetime import datetime, timedelta
 import time
+# import csv to store an array of timestamps of bucket tip events
+import csv
+from typing import Iterable
+
+# removed coma
+DATE_FORMAT = "%d/%m/%Y %H:%M:%S"
+# Volume of water needed to tip rain gague
+BUCKET_SIZE = 0.2794
 
 #Records Bucket Tip
 def bucketTipped():
+<<<<<<< Updated upstream
 	#Formats current date and time as DD/MM/YY, HH:MM:SS
 	dtNow = datetime.datetime.now()
 	appendMe = dtNow.strftime("%d/%m/%Y, %H:%M:%S")
@@ -18,36 +27,61 @@ def cleanRainfall(dateNow):
 	rainFile = open("/home/pi/Documents/Integrated Sensors/rainTimesAll.txt", "r")
 	rainTimes = rainFile.readlines()
 	rainFile.close()
+=======
+	now_timestamp: str = datetime.now().strftime(DATE_FORMAT)
+	with open("rain_tip_times.csv", "a", newline="") as csv_file:
+		csv_writer = csv.writer(csv_file, delimiter=",")
+		csv_writer.writerow([now_timestamp])
+>>>>>>> Stashed changes
 
-	#Sets an empty list to record tip times since midnight
-	validTimes = []
 
-	#Checks a tip exists
-	if len(rainTimes) > 0:
-		for time in rainTimes: #Iterates through each time in the file
-			date = time[0:10] #Extracts the date (first 10 characters)
+def time_difference_less_than_day(now: datetime, then: datetime):
+	difference: timedelta = now - then
+	return difference.days < 1
 
-			#Checks if the date of the tip is today's tip, and if so appends to list of valid tips
-			if date == dateNow:
-				validTimes.append(time)
+#Returns volume of rain in given period of time
+def returnTips():  # sourcery skip: inline-immediately-yielded-variable
+	now: datetime = datetime.now()
 
+<<<<<<< Updated upstream
 		#Writes all valid times to text file, overwriting previously stored tip times
 		recentRainFile = open("/home/pi/Documents/Integrated Sensors/rainTimesAll.txt", "w")
 		for times in validTimes:
 			recentRainFile.write(times)
 		recentRainFile.close()
+=======
+	def generate_tip_timestamps():
+		with open("rain_tip_times.csv", "r") as csv_file:
+			# csv_reader is an iterable of rows
+			csv_reader = csv.reader(csv_file, delimiter=',')
+			for row in csv_reader:
+				time_stamp: str = row[0]
+				time: datetime = datetime.strptime(time_stamp, DATE_FORMAT)
+				yield time
+>>>>>>> Stashed changes
 
-	return len(validTimes) #Returns number of tips recorded
+	tips: Iterable[datetime] = generate_tip_timestamps()
+	# filter where function true
+	tips_in_last_day: list[datetime] = list(filter(
+		lambda then: time_difference_less_than_day(now, then),
+		tips
+	))
+	num_tips_in_last_day: int = len(tips_in_last_day)
+	# print(list(tips_in_last_day))
+	# turn each tip into a single element array to show a row
+	rows: list[str] = list(map(
+		lambda tip: [tip.strftime(DATE_FORMAT)],
+		tips_in_last_day
+	))
 
-#Returns volume of rain in given period of time
-def returnTips():
-	timeNow = datetime.datetime.now()
-	dtForm = timeNow.strftime("%d/%m/%Y, %H:%M:%S") #Formats current date and time as DD/MM/YY, HH:MM:SS
+	# overwrite csv with new filtered tips
+	with open("rain_tip_times.csv", "w", newline="") as csv_file:
+		csv_writer = csv.writer(csv_file, delimiter=",")
+		csv_writer.writerows(rows)
 
-	bucketTips = cleanRainfall(dtForm[0:10]) #Returns number of tips today
-	bucket_size = 0.2794 #Volume of water needed to tip rain gague
+	total_volume = num_tips_in_last_day * BUCKET_SIZE
+	return total_volume
 
-	return bucketTips * bucket_size #Returns volume of rain collected in given period
 
 #Wait for and record bucket tips
 def main():
@@ -56,5 +90,4 @@ def main():
 
 	while time.time() - startTime < ((5 * 60) - 52): #Change RHS for time between readings - minus 52 to account for time taking other readings
 		rain_sensor.when_pressed = bucketTipped
-	volumeCollected = returnTips()
-	return volumeCollected
+	return returnTips()
