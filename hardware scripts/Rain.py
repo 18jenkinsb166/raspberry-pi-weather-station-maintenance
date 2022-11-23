6a86abddf4b4a9073ce1b2a6edd76585b4354971
SAPosner <98704292+SAPosner@users.noreply.github.com>
2022-11-23 16:23:06 +0000

    Fixed Rain.py file bug

--- hardware scripts/Rain.py
+++ hardware scripts/Rain.py
@@ -19,34 +19,32 @@ def cleanRainfall(dateNow):
 	rainTimes = rainFile.readlines()
 	rainFile.close()
 
+	#Sets an empty list to record tip times since midnight
+	validTimes = []
+
 	#Checks a tip exists
 	if len(rainTimes) > 0:
-		flag = True
-		while flag == True:
-			try:
-				date = (rainTimes[0])[0:9] #Takes date from each record
-			except IndexError: #No more dates in file
-				return 0
-			else:
-				if date != dateNow: #Checks date is not current date
-					rainTimes.remove(rainTimes[0])
-				else:
-					flag = False #Breaks loop
+		for time in rainTimes: #Iterates through each time in the file
+			date = time[0:10] #Extracts the date (first 10 characters)
 
-		#Write all valid times to text file, overwriting previously stored tip times
+			#Checks if the date of the tip is today's tip, and if so appends to list of valid tips
+			if date == dateNow:
+				validTimes.append(time)
+
+		#Writes all valid times to text file, overwriting previously stored tip times
 		recentRainFile = open("rainTimesAll.txt", "w")
-		for times in rainTimes:
+		for times in validTimes:
 			recentRainFile.write(times)
 		recentRainFile.close()
 
-	return len(rainTimes) #Retrns number of tips recorded
+	return len(validTimes) #Returns number of tips recorded
 
 #Returns volume of rain in given period of time
 def returnTips():
 	timeNow = datetime.datetime.now()
 	dtForm = timeNow.strftime("%d/%m/%Y, %H:%M:%S") #Formats current date and time as DD/MM/YY, HH:MM:SS
 
-	bucketTips = cleanRainfall(dtForm[0:9]) #Returns number of tips today
+	bucketTips = cleanRainfall(dtForm[0:10]) #Returns number of tips today
 	bucket_size = 0.2794 #Volume of water needed to tip rain gague
 
 	return bucketTips * bucket_size #Returns volume of rain collected in given period
